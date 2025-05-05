0f1304fbe71e384f6801a9446be118aa12f231e8
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-05 13:15:09 +0200

    feat:/function operated successfully

--- src/app.js
+++ src/app.js
@@ -5,3 +5,19 @@ function greet(name) {
     return `Hello, ${name}!`;
   }
   console.log(greet("david"))
+
+  const sayHello = function(num) {
+    return num + 7
+  };
+  console.log(sayHello(4))
+
+// arrow function
+ const welcame = (name) => `welcame ${name}` ;
+ console.log(welcame("diane"))
+ const comment = (name = "danny") => `${name}`
+ console.log(comment());
+
+ function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+  }
+  console.log(sum());
